9611984aaa3eeb6a1768c3c1be3ad05b6e1388c5
SAMBARAN DAS <sambarandasmails@gmail.com>
2023-06-25 12:57:48 +0530

    New file updated

---
+++ Datatypes.java
@@ -0,0 +1,14 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        /*byte mybyteNum = 125;
+        short myShortNum = 512;
+        int myIntNum = 1024;
+        long myLongNum = 50000L + 10L*(mybyteNum + myShortNum + myIntNum);
+        System.out.println(myLongNum);
+        float pound = 200f;
+        double kg = 0.45359237d * pound;
+        System.out.println("My pound value is: "+pound);
+        System.out.println("My kg value is: "+kg);*/
+        
+    }
+}
