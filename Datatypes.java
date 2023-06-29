b708adec16be54dc8318cfaca48688c259ea4af5
SAMBARAN DAS <sambarandasmails@gmail.com>
2023-06-29 12:04:34 +0530

    Method Overloading updated

--- Datatypes.java
+++ Datatypes.java
@@ -1,6 +1,6 @@
 public class Datatypes {
     public static void main(String[] args) {
-        /*byte mybyteNum = 125;
+        byte mybyteNum = 125;
         short myShortNum = 512;
         int myIntNum = 1024;
         long myLongNum = 50000L + 10L*(mybyteNum + myShortNum + myIntNum);
@@ -8,7 +8,7 @@ public class Datatypes {
         float pound = 200f;
         double kg = 0.45359237d * pound;
         System.out.println("My pound value is: "+pound);
-        System.out.println("My kg value is: "+kg);*/
+        System.out.println("My kg value is: "+kg);
         char mycharacter = '\u00AE';
         System.out.println("My unicode is: "+mycharacter);
     }
