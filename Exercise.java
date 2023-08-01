998e47ad85b85ab20d5b39f4de0d68ce53727699
SAMBARAN DAS <sambarandasmails@gmail.com>
2023-08-02 02:32:26 +0530

    Several java file added

--- Exercise.java
+++ Exercise.java
@@ -1,38 +1,27 @@
+import java.util.Scanner;
+
 public class Exercise {
     public static void main(String[] args) {
-        long minutes = 61;
-        long seconds = 0;
-        long total = getDurationString(minutes, seconds);
-        long remainmin = total/60;
-        long remainsec = total%60;
-        long hour = remainmin/60;
-        remainmin = remainmin % 60;
-        if (total == -1){
-            System.out.println("Not a Valid Input");
-        }else{
-        System.out.println(minutes + " minutes and " + seconds + " seconds = " + hour+"h "+ remainmin+"m "+remainsec + "s");
-        }
-    }
-    public static long getDurationString(long minutes, long seconds){
-        long total =0;
-        if (minutes <0 || seconds <0 || seconds >59){
-            return -1;
-        }else if (minutes==0 && seconds != 0){
-            total = seconds;
-        }else if (minutes >0 && seconds ==0){
-            total = minutes*60;
-        }else{
-            total = minutes*60 + seconds;
-        }
-        return total;
-    }
-    public static long getDurationString(long seconds) {
-        if (seconds <0){
-            return -1;
-        }
-        long minutes = seconds/60;
-        seconds = seconds %60;
-        System.out.println(seconds + " Seconds = " + minutes + " Minutes and " + seconds + " Seconds");
-    return getDurationString(minutes, seconds);
+        // Scanner scanner = new Scanner(System.in);
+        // int min=Integer.MAX_VALUE;
+        // int max=Integer.MIN_VALUE;
+        // while(true){
+        //     System.out.println("Enter number: ");
+        //     boolean isannum = scanner.hasNextInt();
+        //     if(isannum){
+        //         int number=scanner.nextInt();
+        //         if(min>number){
+        //             min=number;
+        //         }else if(max<number){
+        //             max=number;
+        //         }
+        //     }else{break;}
+        //     scanner.nextLine();
+        // }
+        // System.out.println("The Minimum number is: "+min);
+        // System.out.println("The Maximum number is: "+max);
+        // scanner.close();
+        long num = Long.MAX_VALUE;
+        System.out.println(num);
     }
 }
