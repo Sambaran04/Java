38d7720a27ab8e2954ea1aff81836ed116a58e79
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-02 00:27:13 +0530

    "Contains Duplicate file created"

--- Problem Solving/src/ContainsDuplicate.java
+++ Problem Solving/src/ContainsDuplicate.java
@@ -1,5 +1,20 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int[] nums = {1,2,3,1};
+        int[] nums = {1, 2, 3, 1};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> intSet = new HashSet<>();
+        for (int num : nums){
+            if(intSet.contains(num))
+                return true;
+
+            intSet.add(num);
+        }
+        return false;
     }
 }
