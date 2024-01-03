9d5ef4c5a580191ac5c73466f79047f079df4625
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-03 10:51:36 +0530

    "Sqare root of x"

---
+++ Problem Solving/src/Sqrt_x.java
@@ -0,0 +1,9 @@
+public class Sqrt_x {
+    public static void main(String[] args) {
+        int x = 8;
+        System.out.println(mySqrt(x));
+    }
+    public static int mySqrt(int x) {
+        return (int) Math.sqrt(x);
+    }
+}
