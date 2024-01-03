f5f8e8e1a69150bdeb7068fc510721ee4d3a7be7
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-03 11:07:51 +0530

    "Climbing Stairs file created, It's a dp problem"

---
+++ Problem Solving/src/ClimbingStairs.java
@@ -0,0 +1,19 @@
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        int n=3;
+        System.out.println(climbStairs(n));
+    }
+    public static int climbStairs(int n) {
+        if (n==1)
+            return 1;
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <=n; i++) {
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr[n];
+
+    }
+}
