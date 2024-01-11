354518e1377be76d2970c3ed16fe692d5eee384f
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-11 22:05:42 +0530

    "Move all the zeros at the end of the array"

--- Problem Solving/src/MoveZeros.java
+++ Problem Solving/src/MoveZeros.java
@@ -4,7 +4,9 @@ public class MoveZeros {
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
         moveZeroes(nums);
-        System.out.println(Arrays.toString(nums));
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]+" ");
+        }
     }
     public static void moveZeroes(int[] nums) {
         int insertPosition = 0;
