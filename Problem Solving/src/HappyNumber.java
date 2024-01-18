307bd64ccc64882b7587542b3477d789aeef4c9d
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-18 23:31:00 +0530

    "Happy Numbers"

---
+++ Problem Solving/src/HappyNumber.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class HappyNumber {
+
+    public static void main(String[] args) {
+        int n=19;
+        System.out.println(isHappy(n));
+    }
+
+    public static boolean isHappy(int n) {
+        Set<Integer> usedInt = new HashSet<>();
+
+        while (true){
+            int sum=0;
+            while(n!=0){
+                sum += Math.pow(n%10, 2);
+                n/=10;
+            }
+            if (sum==1) return true;
+
+            n=sum;
+            if (usedInt.contains(n)) return false;
+            usedInt.add(n);
+        }
+    }
+}
