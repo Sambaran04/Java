4c64619773afdae8d1556649a8b6a24f494e1867
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-26 19:39:07 +0530

    "Bubble sort file created in java"

---
+++ Sorting/src/com/sambaran/BubbleSort.java
@@ -0,0 +1,23 @@
+package com.sambaran;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println("Your array is: "+ Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("Your sorted array is: "+ Arrays.toString(arr));
+    }
+    public static void bubbleSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 1; j < arr.length-i; j++) {
+                if (arr[j]<arr[j-1]){
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
