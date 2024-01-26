9a83bb6bbcba3b2f41f51c4e352d6a7665c00dff
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-26 19:39:26 +0530

    "Selection sort file created in java"

---
+++ Sorting/src/com/sambaran/SelectionSort.java
@@ -0,0 +1,23 @@
+package com.sambaran;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        System.out.println("Your array is: "+ Arrays.toString(arr));
+        selectionSort(arr);
+        System.out.println("Your sorted array is: "+ Arrays.toString(arr));
+    }
+    public static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j]<arr[i]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
