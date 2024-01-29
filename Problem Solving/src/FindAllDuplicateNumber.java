4d2ee553b9391c364225334076b2939e373d693a
SAMBARAN DAS <sambarandasmails@gmail.com>
2024-01-29 22:23:25 +0530

    "Used Cyclic sort here"

--- Problem Solving/src/FindAllDuplicateNumber.java
+++ Problem Solving/src/FindAllDuplicateNumber.java
@@ -4,7 +4,26 @@ import java.util.List;
 public class FindAllDuplicateNumber {
     public static void main(String[] args) {
         int[] nums = {4,3,2,7,8,2,3,1};
-        System.out.println(findDuplicates(nums));
+        System.out.println(findDuplicatesCyclic(nums));
+    }
+//    Used Cyclic Sort here
+    public static List<Integer> findDuplicatesCyclic(int[] nums){
+        int i=0;
+        while (i< nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }else {
+                i++;
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for (int j = 0; j < nums.length; j++) {
+            if (nums[j] != j+1){
+                ans.add(nums[j]);
+            }
+        }
+        return ans;
     }
     public static List<Integer> findDuplicates(int[] nums) {
         List<Integer> result = new ArrayList<>();
@@ -14,5 +33,11 @@ public class FindAllDuplicateNumber {
             else result.add(n);
         }
         return result;
+    }
+
+    public static void swap(int[] arr, int start, int end){
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
     }
 }
