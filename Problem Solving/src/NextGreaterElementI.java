8965d273c1e642fe2ecc66ba9aa5a3f77393b5c4
Sambaran-ClearSpot <sambaran@clearspot.ai>
2024-03-12 00:40:21 +0530

    "Next Greater Element I"

--- Problem Solving/src/NextGreaterElementI.java
+++ Problem Solving/src/NextGreaterElementI.java
@@ -1,28 +1,74 @@
-import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
 
 public class NextGreaterElementI {
     public static void main(String[] args) {
-        int[] nums1 = {4,1,2};
-        int[] nums2 = {1,3,4,2};
+        int[] nums1 = {1,3,5,2,4};
+        int[] nums2 = {6,5,4,3,2,1,7};
         System.out.println(Arrays.toString(nextGreaterElement(nums1, nums2)));
     }
+
     public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int[] ans = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            for (int j = 0; j < nums2.length; j++) {
-                if (nums1[i]==nums2[j]){
-                    // Check if nums2[j+1]>nums1[i], if yes append the number else append -1
-                    if (j == nums2.length-1){
-                        ans[i] = -1;
-                    }else if (nums2[j+1] < nums2[j]){
-                        ans[i] = -1;
-                    }else {
-                        ans[i] = nums2[j+1];
-                    }
-                }
+
+        if (nums2.length == 0 || nums1.length == 0)
+            return new int[0];
+
+        Map<Integer, Integer> numberNGE = new HashMap<>();
+        Stack<Integer> numStack = new Stack<>();
+
+        numStack.push(nums2[nums2.length - 1]);
+        numberNGE.put(nums2[nums2.length - 1], -1);
+
+        for (int i = nums2.length - 2; i >= 0; i--) {
+
+            if (nums2[i] < numStack.peek()) {
+                numberNGE.put(nums2[i], numStack.peek());
+                numStack.push(nums2[i]);
+                continue;
+            }
+
+            while (!numStack.isEmpty() && numStack.peek() < nums2[i])
+                numStack.pop();
+
+            if (numStack.isEmpty()) {
+                numStack.push(nums2[i]);
+                numberNGE.put(nums2[i], -1);
+            } else {
+                numberNGE.put(nums2[i], numStack.peek());
+                numStack.push(nums2[i]);
             }
         }
-        return ans;
+
+        for (int i = 0; i < nums1.length; i++)
+            nums1[i] = numberNGE.get(nums1[i]);
+
+        return nums1;
     }
+    /*public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums1.length; i++) {
+            int index = -1;
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j]) {
+                    index = j;
+                    break;
+                }
+            }
+            if (index == -1) {
+                result.add(-1);
+            } else {
+                int nextGreater = -1;
+                for (int j = index + 1; j < nums2.length; j++) {
+                    if (nums2[j] > nums1[i]) {
+                        nextGreater = nums2[j];
+                        break;
+                    }
+                }
+                result.add(nextGreater);
+            }
+        }
+        return result.stream().mapToInt(i -> i).toArray();
+    }*/
 }
