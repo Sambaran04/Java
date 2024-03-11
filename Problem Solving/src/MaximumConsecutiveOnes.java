52cbbfde67ab0ea2a5711198690a0dbfbb7169bb
Sambaran-ClearSpot <sambaran@clearspot.ai>
2024-03-11 23:36:11 +0530

    "Maximum Consecutive ones in an array Changed with enhanced for loop"

--- Problem Solving/src/MaximumConsecutiveOnes.java
+++ Problem Solving/src/MaximumConsecutiveOnes.java
@@ -7,11 +7,11 @@ public class MaximumConsecutiveOnes {
     public int findMaxConsecutiveOnes(int[] nums) {
         int max=0;
         int count =0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i]==1){
+        for (int num : nums) {
+            if (num == 1) {
                 count++;
                 max = Math.max(max, count);
-            }else count=0;
+            } else count = 0;
         }
         return max;
     }
