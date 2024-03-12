fc89fa92fa82d4047a72dd36b8cb00e5d55ac142
Sambaran04 <sambarandasmails@gmail.com>
2024-03-12 18:50:04 +0530

    "Pattern 7"

---
+++ Pattern/src/Pattern7.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the value of n: ");
+        int n = scanner.nextInt();
+        for (int i = 1; i <= 2*n; i++) {
+
+            int c = i > n ? 2*n-i : i;
+
+            for (int j = 0; j < n-c; j++) {
+                System.out.print("  ");
+            }
+            for (int j = c; j >=1 ; j--) {
+                System.out.print(j+ " ");
+            }
+            for (int j = 2; j <= c; j++) {
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
