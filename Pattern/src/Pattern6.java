c60ce36f002f3b6f3a59ee024656fb7052af42ef
Sambaran04 <sambarandasmails@gmail.com>
2024-03-12 18:48:13 +0530

    "Pattern 6"

--- Pattern/src/Pattern6.java
+++ Pattern/src/Pattern6.java
@@ -5,14 +5,17 @@ public class Pattern6 {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter the value of n: ");
         int n = scanner.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < n-i; j++) {
+        for (int i = 1; i <= 2*n; i++) {
+
+            int c = i > n ? 2*n-i : i;
+
+            for (int j = 0; j < n-c; j++) {
                 System.out.print("  ");
             }
-            for (int j = i; j >=1 ; j--) {
+            for (int j = c; j >=1 ; j--) {
                 System.out.print(j+ " ");
             }
-            for (int j = 2; j <= i; j++) {
+            for (int j = 2; j <= c; j++) {
                 System.out.print(j+" ");
             }
             System.out.println();
