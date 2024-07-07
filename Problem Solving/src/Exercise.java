ed46ce2417373188f3d9ba95be0c277d74df1fbf
Sambaran04 <sambarandasmails@gmail.com>
2024-07-08 00:19:38 +0530

    "Practiced DSA"

--- Problem Solving/src/Exercise.java
+++ Problem Solving/src/Exercise.java
@@ -1,19 +1,5 @@
 public class Exercise {
     public static void main(String[] args) {
-        int num =121;
-        System.out.println(isPalindrome(num));
-    }
-    public static boolean isPalindrome(int x) {
-        String a = Integer.toString(x);
-        int i=0;
-        int j = a.length()-1;
-        while(i<j){
-            if(a.charAt(i) != a.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
+        System.out.print("Hello World");
     }
 }
