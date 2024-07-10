3f8aa19fb60f527aef2d9f1c6c3394966c872548
Sambaran04 <sambarandasmails@gmail.com>
2024-07-11 03:35:01 +0530

    "Bubble Sort"

--- Sorting/src/com/sambaran/BubbleSort.java
+++ Sorting/src/com/sambaran/BubbleSort.java
@@ -11,13 +11,16 @@ public class BubbleSort {
     }
     public static void bubbleSort(int[] arr){
         for (int i = 0; i < arr.length; i++) {
+            int count =0;
             for (int j = 1; j < arr.length-i; j++) {
                 if (arr[j]<arr[j-1]){
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
+                    count++;
                 }
             }
+            if (count==0)break;
         }
     }
 }
