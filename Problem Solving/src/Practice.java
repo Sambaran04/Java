bac5f2ec5a14e64c9c0534530227525f62fa2534
Sambaran04 <sambarandasmails@gmail.com>
2024-07-11 03:37:24 +0530

    "Practice sort file created in java"

--- Problem Solving/src/Practice.java
+++ Problem Solving/src/Practice.java
@@ -1,5 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        int[] arr = {10, 20, 50, 5, 1};
+        int max=0;
+        int smax=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>max){
+                smax = max;
+                max = arr[i];
+            }
+        }
+        System.out.println(smax);
     }
 }
