b85ad672ea140074db4a4465ebe2864f8be71e1e
Sambaran04 <sambarandasmails@gmail.com>
2024-07-11 03:35:35 +0530

    "Practice Sort"

--- Sorting/src/com/sambaran/Practice.java
+++ Sorting/src/com/sambaran/Practice.java
@@ -6,8 +6,26 @@ public class Practice {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
         System.out.println("Array before sorting: "+Arrays.toString(arr));
-        cyclicSort(arr);
+//        cyclicSort(arr);
+//        selection(arr);
+        bubble(arr);
         System.out.println("Array after sorting: "+Arrays.toString(arr));
+    }
+
+    public static void selection(int[] arr){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                swap(arr, i, j);
+            }
+        }
+    }
+
+    public static void bubble(int[] arr){
+        for (int i=0; i<arr.length; i++){
+            for (int j=1; j<arr.length-i; j++){
+                if(arr[j-1]>arr[j]) swap(arr, j-1, j);
+            }
+        }
     }
     public static void cyclicSort(int[] arr){
         int i=0;
