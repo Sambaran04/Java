d13880aac5a7778192bee54137a6df9fa1473ec4
Sambaran04 <sambarandasmails@gmail.com>
2024-07-11 03:35:19 +0530

    "Insertion Sort"

--- Sorting/src/com/sambaran/InsertionSort.java
+++ Sorting/src/com/sambaran/InsertionSort.java
@@ -17,7 +17,7 @@ public class InsertionSort {
                     int temp = arr[j-1];
                     arr[j-1] = arr[j];
                     arr[j] = temp;
-                }else break;
+                }   else break;
             }
         }
     }
