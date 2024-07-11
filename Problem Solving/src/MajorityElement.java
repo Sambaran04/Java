47bc1eeb3896cbbdf9e9c96402ee300309ca5c6a
Sambaran04 <sambarandasmails@gmail.com>
2024-07-11 22:59:34 +0530

    "Majority Element"

--- Problem Solving/src/MajorityElement.java
+++ Problem Solving/src/MajorityElement.java
@@ -7,12 +7,26 @@ public class MajorityElement {
         int n = nums.length;
         HashMap<Integer, Integer> mp = new HashMap<>();
 
-        for(int i=0; i<n; i++){
-            int value = mp.getOrDefault(nums[i], 0);
-            mp.put(nums[i], value+1);
+        for (int num : nums) {
+            int value = mp.getOrDefault(num, 0);
+            mp.put(num, value + 1);
         }
         for(Map.Entry<Integer, Integer> it:mp.entrySet()){
             if (it.getValue()>(n/2)) System.out.println(it.getKey());
         }
+    }
+    public int majorityElement(int[] nums) {
+        int major = nums[0], vote=1;
+        for(int i=1; i<nums.length; i++){
+            if(vote==0){
+                vote++;
+                major = nums[i];
+            }else if(major == nums[i]){
+                vote++;
+            }else{
+                vote--;
+            }
+        }
+        return major;
     }
 }
