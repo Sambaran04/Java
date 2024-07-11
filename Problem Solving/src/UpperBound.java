3116eb7ca202f678bcf554ab873c2c87c47ff64d
Sambaran04 <sambarandasmails@gmail.com>
2024-07-12 01:59:33 +0530

    "upper Bound of the sorted array"

---
+++ Problem Solving/src/UpperBound.java
@@ -0,0 +1,26 @@
+public class UpperBound {
+    public static void main(String[] args) {
+        long[] arr = {1, 2, 5, 6, 10};
+        long x = 3;
+        System.out.println(findUpper(arr, arr.length, x));
+    }
+    static int findUpper(long[] arr, int n, long x) {
+        int left = 0, right = n - 1;
+        int ceilIndex = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] < x) {
+                left = mid + 1;
+            } else {
+                ceilIndex = mid;
+                right = mid - 1;
+            }
+        }
+
+        return ceilIndex;
+    }
+}
