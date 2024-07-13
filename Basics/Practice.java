4405a3183e3661986c6390feb73486e94a2000c1
Sambaran04 <sambarandasmails@gmail.com>
2024-07-13 20:21:21 +0530

    "Practice a lot"

--- Basics/Practice.java
+++ Basics/Practice.java
@@ -1,11 +1,34 @@
+import java.util.Arrays;
+
 public class Practice {
     public static void main(String[] args) {
-        int n = 12345;
-        int count = 0;
-        while (n > 0) {
-            n = n / 10;
-            count++;
+        int[] arr = {2, 4, 1, 3, 5};
+        int index=-1;
+        int max=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+                index = i;
+            }
         }
-        System.out.println(count);
+        arr[index] = 0;
+        index=-1;
+        max=0;
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+                index = i;
+            }
+        }
+        arr[index] = 0;
+        index=-1;
+        max=0;
+        System.out.println(Arrays.toString(arr));
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
     }
 }
