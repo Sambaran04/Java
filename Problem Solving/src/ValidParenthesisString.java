dadde92e38b7d574ee3cbed57cfaefad14b97d5f
Sambaran04 <sambarandasmails@gmail.com>
2024-07-18 04:18:29 +0530

    "Valid Parenthesis String"

---
+++ Problem Solving/src/ValidParenthesisString.java
@@ -0,0 +1,26 @@
+public class ValidParenthesisString {
+    public boolean checkValidString(String s) {
+        int min=0, max=0;
+        for (char c: s.toCharArray()){
+            if(c == '('){
+                min++;
+                max++;
+            }
+            else if(c==')'){
+                min--;
+                max--;
+            }
+            else {
+                min--;
+                max++;
+            }
+            if (min<0){
+                min=0;
+            }
+            if (max<0){
+                return false;
+            }
+        }
+        return (min==0);
+    }
+}
