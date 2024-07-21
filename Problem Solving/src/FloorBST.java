04ae8b5b56c5d998aa87e2c913a63752860128ca
Sambaran04 <sambarandasmails@gmail.com>
2024-07-22 02:33:20 +0530

    "Floor value of a given value in BST"

---
+++ Problem Solving/src/FloorBST.java
@@ -0,0 +1,22 @@
+public class FloorBST {
+    public static int floor(TreeNode root, int x) {
+        if(root==null){
+            return -1;
+        }
+        int floor=-1;
+        while(root!=null){
+            if(x==root.val){
+                floor = root.val;
+                return floor;
+            }
+            if(root.val>x){
+                root = root.left;
+            }
+            else{
+                floor = root.val;
+                root = root.right;
+            }
+        }
+        return floor;
+    }
+}
