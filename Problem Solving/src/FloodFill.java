84deabe52914acb6ad73b4a21cacb5fa818addb6
Sambaran04 <sambarandasmails@gmail.com>
2024-07-25 18:51:51 +0530

    "Flood Fill in leetcode using DFS and BFS"

---
+++ Problem Solving/src/FloodFill.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class FloodFill {
+    public int[][] floodFillBFS(int[][] image, int sr, int sc, int color) {
+        if (image==null || image.length==0 ||image[sr][sc] == color) return image;
+        int rows = image.length;
+        int cols = image[0].length;
+        int init = image[sr][sc];
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(sr, sc));
+        image[sr][sc] = color;
+
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+        while (!q.isEmpty()) {
+            Pair point = q.poll();
+            int r = point.row;
+            int c = point.col;
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + drow[i];
+                int ncol = c + dcol[i];
+                if (nrow >= 0 && ncol >= 0 && nrow < rows && ncol < cols && image[nrow][ncol] == init) {
+                    image[nrow][ncol] = color;
+                    q.add(new Pair(nrow, ncol));
+                }
+            }
+        }
+        return image;
+    }
+
+    public int[][] floodFillDFS(int[][] image, int sr, int sc, int color) {
+        int init = image[sr][sc];
+        int[][] vis = image;
+        int[] drow = {-1, 0, 1, 0};
+        int[] dcol = {0, 1, 0, -1};
+        dfs(sr, sc, vis, image, drow, dcol,color, init);
+        return vis;
+    }
+    public static void dfs(int sr, int sc, int[][] vis, int[][] image, int[] drow, int[] dcol, int color, int init){
+        vis[sr][sc] = color;
+        int rows = image.length;
+        int cols = image[0].length;
+        for (int i = 0; i < 4; i++) {
+            int nrow = sr + drow[i];
+            int ncol = sc + dcol[i];
+            if (nrow >= 0 && ncol >= 0 && nrow < rows && ncol < cols && image[nrow][ncol] == init && vis[nrow][ncol] !=color){
+                dfs(nrow, ncol, vis, image, drow, dcol, color, init);
+            }
+        }
+    }
+}
+public class Pair{
+    public int row;
+    public int col;
+
+    public Pair(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+}
