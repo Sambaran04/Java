942e1afdd32f6792f05ec8b4411bb10ac3cd6ae9
Sambaran04 <sambarandasmails@gmail.com>
2024-07-30 23:50:02 +0530

    "Union Two Sorted Arrays"

--- Problem Solving/src/UnionTwoSortedArrays.java
+++ Problem Solving/src/UnionTwoSortedArrays.java
@@ -1,31 +1,41 @@
 import java.util.ArrayList;
 
 public class UnionTwoSortedArrays {
-    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m)
-    {
-        ArrayList<Integer> ans = new ArrayList<Integer>();
-        int i=0, j = 0, k=0;
-        while(i<n && j<m) {
-            if (arr1[i] <= arr2[j]) {
-                ans.add(k, arr1[i]);
+    public static void main(String[] args) {
+        int[] arr1 = {1, 2, 3, 4, 5};
+        int[] arr2 = {1, 2, 3};
+        int n = 5, m = 3;
+        System.out.println(findUnion(arr1, arr2, n, m));
+    }
+
+    static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m) {
+        int i = 0, j = 0; // pointers
+        ArrayList<Integer> Union = new ArrayList<>(); // Uninon vector
+        while (i < n && j < m) {
+            if (arr1[i] <= arr2[j]) // Case 1 and 2
+            {
+                if (Union.isEmpty() || Union.getLast() != arr1[i])
+                    Union.add(arr1[i]);
                 i++;
-                k++;
-            } else {
-                ans.add(k, arr2[j]);
-                k++;
+            } else // case 3
+            {
+                if (Union.isEmpty() || Union.getLast() != arr2[j])
+                    Union.add(arr2[j]);
                 j++;
             }
         }
-        while(i<n){
-            ans.add(k, arr1[i]);
+        while (i < n) // IF any element left in arr1
+        {
+            if (Union.getLast() != arr1[i])
+                Union.add(arr1[i]);
             i++;
-            k++;
         }
-        while(j<m){
-            ans.add(k, arr2[j]);
+        while (j < m) // If any elements left in arr2
+        {
+            if (Union.getLast() != arr2[j])
+                Union.add(arr2[j]);
             j++;
-            k++;
         }
-        return ans;
+        return Union;
     }
 }
