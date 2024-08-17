b764bae19d2e2ec3730fedb125685d71cca4383e
Sambaran04 <sambarandasmails@gmail.com>
2024-08-17 14:20:18 +0530

    "Find the Kth Rotation"

--- Problem Solving/src/FindKRotation.java
+++ Problem Solving/src/FindKRotation.java
@@ -14,23 +14,34 @@ public class FindKRotation {
     public static int findKRotation(List<Integer> arr) {
         int start = 0;
         int end = arr.size()-1;
-        if (arr.size()==1) return arr.get(0);
-        else if (arr.get(0)>arr.get(1)) return arr.get(0);
-        else if (arr.getLast()>(arr.size()-2)) {
-            return arr.getLast();
-        }
-        else {
-            while (start <= end) {
-                int mid = start + (end - start) / 2;
-                if (arr.get(mid) > arr.get(mid - 1) && arr.get(mid) > arr.get(mid + 1)) {
-                    return mid;
-                } else if (arr.get(mid) > arr.get(mid - 1)) {
-                    start = mid + 1;
-                } else {
-                    end = mid - 1;
+        if (arr.size()==1) return 0;
+        int index=-1;
+        int ans = Integer.MAX_VALUE;
+        while (start<=end){
+            int mid = start + (end-start)/2;
+            if (arr.get(start)<=arr.get(end)){
+                if (arr.get(start)<ans){
+                    index = start;
+                    ans = arr.get(start);
                 }
+                break;
+            }
+            if (arr.get(start)<=arr.get(mid)){
+                if (arr.get(start)<ans){
+                    index = start;
+                    ans = arr.get(start);
+                }
+                start = mid+1;
+            }
+            else {
+                if (arr.get(mid)<ans){
+                    index = mid;
+                    ans = arr.get(mid);
+                }
+                end = mid-1;
+
             }
         }
-        return -1;
+        return index;
     }
 }
