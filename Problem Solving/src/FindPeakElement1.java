2b9c86258e3458b99df1f882b698a4dea33d8180
Sambaran04 <sambarandasmails@gmail.com>
2024-08-20 01:06:29 +0530

    "Find the Peak Element"

--- Problem Solving/src/FindPeakElement1.java
+++ Problem Solving/src/FindPeakElement1.java
@@ -1,6 +1,4 @@
-import java.util.Vector;
-
-public class FindPeakElement {
+public class FindPeakElement1 {
     public static void main(String[] args) {
         int[] arr = {1,2,1,3,5,6,4};
         System.out.println(peak2(arr));
