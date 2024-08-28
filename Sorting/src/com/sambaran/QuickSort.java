d304047e383ab2e6f19d21f1ff091e8213fc15e6
Sambaran04 <sambarandasmails@gmail.com>
2024-08-28 22:52:40 +0530

    "quick sort"

--- Sorting/src/com/sambaran/QuickSort.java
+++ Sorting/src/com/sambaran/QuickSort.java
@@ -1,38 +1,54 @@
 package com.sambaran;
 
+import java.util.Arrays;
+
+import static com.sambaran.Swap.swap;
+
 public class QuickSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        quickSort(arr, 0, arr.length-1);
-        for (int i:arr){
-            System.out.print(i + " ");
+        System.out.println("Array before sorting: " + Arrays.toString(arr));
+        quickSort(arr, 0, arr.length - 1);
+        System.out.println("Array after sorting: " + Arrays.toString(arr));
+    }
+
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int q = hoar(arr, l, r);
+            quickSort(arr, l, q); // for Hoar Method
+//            quickSort(arr, l, q-1); // For Lomuto Method
+            quickSort(arr, q + 1, r);
         }
     }
 
-    public static int partition(int[] arr, int l, int r){
+    public static int lomuto(int[] arr, int l, int r) {
         int x = arr[r];
-        int i = l-1;
-        for (int j = l; j <= r-1; j++) {
-            if (arr[j]<=x){
+        int i = l - 1;
+        for (int j = l; j < r; j++) {
+            if (arr[j] <= x) {
                 i++;
                 swap(arr, i, j);
             }
         }
-        swap(arr, i+1, r);
-        return i+1;
+        swap(arr, i + 1, r);
+        return i + 1;
     }
 
-    public static void quickSort(int[] arr, int l, int r){
-        if (l<r){
-            int p = partition(arr, l, r);
-            quickSort(arr, l, p-1);
-            quickSort(arr, p+1, r);
+    public static int hoar(int[] arr, int l, int r) {
+        int x = arr[l];
+        int i = l - 1;
+        int j = r + 1;
+        while (true) {
+            do {
+                i++;
+            } while (arr[i] < x);
+            do {
+                j--;
+            } while (arr[j] > x);
+            if (i >= j) {
+                return j;
+            }
+            swap(arr, i, j);
         }
-    }
-
-    public static void swap(int[]arr, int i, int j){
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
     }
 }
