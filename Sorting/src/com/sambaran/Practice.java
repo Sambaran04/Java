e953c7cb7b78e1312c619ba435edcc4d5d8c2129
Sambaran04 <sambarandasmails@gmail.com>
2024-08-28 22:50:09 +0530

    "Practice"

--- Sorting/src/com/sambaran/Practice.java
+++ Sorting/src/com/sambaran/Practice.java
@@ -2,44 +2,58 @@ package com.sambaran;
 
 import java.util.Arrays;
 
+import static com.sambaran.Swap.swap;
+
 public class Practice {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
-        System.out.println("Array before sorting: "+Arrays.toString(arr));
-//        cyclicSort(arr);
-//        selection(arr);
-        bubble(arr);
-        System.out.println("Array after sorting: "+Arrays.toString(arr));
+//        int[] arr = {5, 4, 3, 2, 1};
+//        System.out.println("Array before sorting: " + Arrays.toString(arr));
+//        mergeSort(arr, 0, arr.length - 1);
+//        System.out.println("Array after sorting: " + Arrays.toString(arr));
+        System.out.println((int) 'a');
     }
 
-    public static void selection(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j < arr.length; j++) {
-                swap(arr, i, j);
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    public static void merge(int[] arr, int low, int mid, int high) {
+        int ls = mid - low + 1;
+        int rs = high - mid;
+        int[] left = new int[ls];
+        int[] right = new int[rs];
+        for (int i = 0; i < ls; i++) {
+            left[i] = arr[low + i];
+        }
+        for (int i = 0; i < rs; i++) {
+            right[i] = arr[mid + i + 1];
+        }
+        int i = 0, j = 0, k = low;
+        while (i < ls && j < rs) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+                k++;
+            } else {
+                arr[k] = right[j];
+                j++;
+                k++;
             }
         }
-    }
-
-    public static void bubble(int[] arr){
-        for (int i=0; i<arr.length; i++){
-            for (int j=1; j<arr.length-i; j++){
-                if(arr[j-1]>arr[j]) swap(arr, j-1, j);
-            }
+        while (i < ls) {
+            arr[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < rs) {
+            arr[k] = right[j];
+            j++;
+            k++;
         }
     }
-    public static void cyclicSort(int[] arr){
-        int i=0;
-        while(i< arr.length){
-            int correct = arr[i]-1;
-            if (arr[i] != arr[correct]){
-                swap(arr, i, correct);
-            }else i++;
-        }
-    }
-    public static void swap(int[] arr, int start, int end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-    }
-
 }
