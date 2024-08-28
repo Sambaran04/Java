5e5305ca9420c9b59c4568b758bf31021d1c1b93
Sambaran04 <sambarandasmails@gmail.com>
2024-08-28 22:55:16 +0530

    "Selection Sort"

--- Sorting/src/com/sambaran/SelectionSort.java
+++ Sorting/src/com/sambaran/SelectionSort.java
@@ -13,9 +13,7 @@ public class SelectionSort {
         for (int i = 0; i < arr.length-1; i++) {
             for (int j = i+1; j < arr.length; j++) {
                 if (arr[j]<arr[i]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
+                    Swap.swap(arr, i, j);
                 }
             }
         }
