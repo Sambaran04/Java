1b7d223fed71c41adc1f4f2eae02a717ab3c56ca
Sambaran04 <sambarandasmails@gmail.com>
2024-08-28 22:48:27 +0530

    "Insertion Sort"

--- Sorting/src/com/sambaran/InsertionSort.java
+++ Sorting/src/com/sambaran/InsertionSort.java
@@ -2,22 +2,22 @@ package com.sambaran;
 
 import java.util.Arrays;
 
+import static com.sambaran.Swap.swap;
+
 public class InsertionSort {
     public static void main(String[] args) {
         int[] arr = {5, 4, 3, 2, 1};
-        System.out.println("Your array is: "+ Arrays.toString(arr));
+        System.out.println("Your array is: " + Arrays.toString(arr));
         insertionSort(arr);
-        System.out.println("Your sorted array is: "+ Arrays.toString(arr));
+        System.out.println("Your sorted array is: " + Arrays.toString(arr));
     }
-    
-    public static void insertionSort(int[] arr){
+
+    public static void insertionSort(int[] arr) {
         for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j > 0; j--) {
-                if (arr[j]<arr[j-1]){
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }   else break;
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j-1, j);
+                } else break;
             }
         }
     }
