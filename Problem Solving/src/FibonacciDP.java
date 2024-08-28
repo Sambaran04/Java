e9f38364e5afeaaf0d4de41209a141868acc5a21
Sambaran04 <sambarandasmails@gmail.com>
2024-08-28 22:47:37 +0530

    "Fibonacci with Dynamic Programming"

--- Problem Solving/src/FibonacciDP.java
+++ Problem Solving/src/FibonacciDP.java
@@ -1,29 +1,50 @@
 public class FibonacciDP {
     static final int MOD = 1000000007;
 
-    static long topDown(int n) {
-        return fib(n, new long[n+1]);
-    }
-    static long fib(int n, long[] dp){
-        if(n<=1){
-            return (long) n;
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i+" :" +bottomUp2(i));
         }
-        if(dp[n] !=0){
-            return dp[n];
-        }
-        return dp[n] = (long) (fib(n-1, dp)+fib(n-2, dp)) %MOD;
     }
 
-    static long bottomUp(int n) {
-        if(n<=1){
+    static long topDown(int n) {
+        return fib(n, new long[n + 1]);
+    }
+
+    static long fib(int n, long[] dp) {
+        if (n <= 1) {
             return (long) n;
         }
-        int[] dp = new int[n+1];
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        return dp[n] = (long) (fib(n - 1, dp) + fib(n - 2, dp)) % MOD;
+    }
+
+    static long bottomUp1(int n) {
+        if (n <= 1) {
+            return (long) n;
+        }
+        int[] dp = new int[n + 1];
         dp[0] = 0;
         dp[1] = 1;
-        for(int i=2; i<=n; i++){
-            dp[i] = (dp[i-1]+dp[i-2]) %MOD;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
         }
         return (long) dp[n];
+    }
+
+    static long bottomUp2(int n) {
+        if (n <= 1) {
+            return (long) n;
+        }
+        int val1 = 0;
+        int val2 = 1;
+        for (int i = 2; i <n; i++) {
+            int ans = val1 + val2;
+            val1 = val2;
+            val2 = ans;
+        }
+        return (long) val2;
     }
 }
